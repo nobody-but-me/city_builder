0fd41b996a0962140ad7b3ed9a20e8de4151d19d
Daniel Nunes Teixeira <nunesteixeiradaniel12@gmail.com>
2025-04-19 12:20:46 -0300

    Game loop, mouse click detecting over rocks

--- components/Tiles.tsx
+++ components/Tiles.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React, { useEffect, useState } from 'react';
 
 import house from "@/assets/house_tile.webp";
 import grass from "@/assets/grass_tile.webp";
@@ -13,16 +14,36 @@ export default function Tiles() {
     const _MAX_TILE_COLS: string = 'grid-cols-4';
     
     const _standard_tile_animation: string = "tile transform h-38 w-24 hover:transition duration-500 hover:scale-125 flex justify-center items-center";
-    const _tiles = [
-	grass.src, rock.src, pine.src , grass.src,
-	tree.src , grass.src, rock.src, tree.src ,
-	rock.src, pine.src , grass.src, grass.src,
+    const _tiles_map = [
+	grass.src, rock.src , pine.src , grass.src,
+	tree.src , grass.src, rock.src , tree.src ,
+	rock.src , pine.src , grass.src, grass.src,
     ];
+    
+    // TODO: perhaps there are better ways to do the game loop.
+    useEffect(() => {
+	var _tiles = document.getElementsByClassName('tile');
+	
+	const _game_loop = () => {
+	    // this does not seem right.
+	    for(let _i = 0; _i < _tiles.length; _i++) {
+		_tiles[_i].onmousedown = () => {
+		    const _tile = _tiles[_i];
+		    
+		    if (_tile.alt === rock.src) {
+			_tile.src = grass.src;
+		    }
+		};
+	    }
+	    window.requestAnimationFrame(_game_loop);
+	};
+	_game_loop();
+    }, []);
     
     return (
 	<main className={`grid ${_MAX_TILE_COLS} gap-1`}>
 	    {
-		_tiles.map((_i, _j) => {
+		_tiles_map.map((_i, _j) => {
 		    return (
 			<img
 		            height={100}
@@ -30,8 +51,8 @@ export default function Tiles() {
 		                    
 		            key={_j}
 		            src={_i}
-		            alt="_tile_"
-		                    
+		            alt={_i}
+		            
 		            className={_standard_tile_animation}
 		            style={{imageRendering: 'pixelated'}}
 			/>
