710d5555eb9669147b4580c7c6c660a1fd3eeedf
Daniel Nunes Teixeira <nunesteixeiradaniel12@gmail.com>
2025-04-25 18:07:41 -0300

    So many things I can't really remember

--- components/Inventory.tsx
+++ components/Inventory.tsx
@@ -8,6 +8,7 @@ import tree_tile  from "@/public/saplings/tree_sapling0.webp";
 import house_tile from "@/public/house_tile.webp";
 import tent_tile  from "@/public/tent_tile.webp";
 
+import grass_tile from "@/public/grass_tile.webp";
 import item_slot from "@/public/item_slot.webp";
 import rock_item from "@/public/rock_item.webp";
 import wood_item from "@/public/wood_item.webp";
@@ -62,89 +63,101 @@ export function RemoveItem(_item_: string, _quantity_: number) {
 }
 
 export default function Inventory() {
-    
     useEffect(() => {
-	let _building = document.getElementById('building');
-	let _tool = document.getElementById('tool');
-	_tool.addEventListener('mousedown', () => {
-	    if (_current_selected_tool < (_tools.length - 1)) {
-		_current_selected_tool += 1;
-	    } else {
-		_current_selected_tool = 0;
-	    }
-	    _tool.src = _tools[_current_selected_tool];
-	    
-	    if (_current_selected_tool === 0) {
-		_building.classList.remove('hidden');
-	    }
-	    else {
-		_building.classList.add('hidden');
-	    }
-	    
-	    _building.addEventListener('mousedown', () => {
-		if (_current_building < (_buildings.length - 1)) {
-		    _current_building += 1;
-		} else {
-		    _current_building = 0;
-		}
-		_building.src = _buildings[_current_building][0];
+	    let _building = document.querySelectorAll('.building');
+	    let _tool = document.getElementById('tool');
+
+	    _tool.addEventListener('mousedown', () => {
+
+	        if (_current_selected_tool < (_tools.length - 1)) {
+		        _current_selected_tool += 1;
+	        } else {
+		        _current_selected_tool = 0;
+	        }
+	        _tool.src = _tools[_current_selected_tool];
+
+
+            _building.forEach(_b => {
+	            if (_current_selected_tool === 0) {
+                    _b.classList.remove('hidden');
+	            } else {
+		            _b.classList.add('hidden');
+	            }
+            });
+	        document.getElementById('bbutton').addEventListener('mousedown', () => {
+		        if (_current_building < (_buildings.length - 1)) {
+		            _current_building += 1;
+		        } else {
+		            _current_building = 0;
+		        }
+		        document.getElementById('bbutton').src = _buildings[_current_building][0];
+	        });
 	    });
-	});
     }, []);
-    
+
+
     return (<>
-        <div className={`absolute left-0 top-0 p-8 origin-top-left grid grid-rows-5 gap-2 w-50`}>
-    	    <div className={`flex justify-center flex-row items-center gap-3 w-full`}>
-                <img
-                    src={wood_item.src}
-                    height={100}
-                    width={100}
-                    alt='_rock_item_'
-                    className={``}
-                />
-                <p id='wood-text' className={`w-full`}>
-                    {_items['wood']}
-                </p>
-    	    </div>
-    	    <div className={`flex justify-center flex-row items-center gap-3 w-full`}>
-                <img
-                    src={rock_item.src}
-                    height={100}
-                    width={100}
-                    alt='_rock_item_'
-                    className={``}
-                />
-                <p id='rock-text' className={`w-full`}>
-                    {_items['rock']}
-                </p>
-    	    </div>
-        </div>
-	
-        <img
-            src={item_slot.src}
-            height={100}
-            width={100}
-            alt='_item_slot_'
-            className={`absolute right-0 bottom-0`}
-	    style={{right: '10px', bottom: '10px'}}
-        />
-        <img
-            src={_tools[_current_selected_tool]}
-            height={100}
-            width={100}
-            alt='_item_'
-	    id='tool'
-            className={`absolute right-0 bottom-0 hover:transition duration-200 hover:scale-125 flex justify-center items-center`}
-	    style={{right: '10px', bottom: '10px'}}
-        />
-	<img 
-            src={_buildings[_current_building][0]}
-            height={100}
-            width={100}
-            alt='_building_'
-	    id='building'
-            className={`hidden absolute bottom-0 hover:transition duration-200 hover:scale-125 flex justify-center items-center`}
-	    style={{right: '110px', bottom: '10px'}}
-	/>
-    </>)
+               <div className={`absolute left-0 top-0 p-8 origin-top-left grid grid-rows-5 gap-2 w-50`}>
+    	          <div className={`flex justify-center flex-row items-center gap-3 w-full`}>
+               <img
+                   src={wood_item.src}
+                   height={100}
+                   width={100}
+                   alt='_rock_item_'
+                   className={``}
+               />
+               <p id='wood-text' className={`w-full`}>
+               {_items['wood']}
+                   </p>
+    	          </div>
+    	          <div className={`flex justify-center flex-row items-center gap-3 w-full`}>
+               <img
+                   src={rock_item.src}
+                   height={100}
+                   width={100}
+                   alt='_rock_item_'
+                   className={``}
+               />
+               <p id='rock-text' className={`w-full`}>
+               {_items['rock']}
+                   </p>
+    	          </div>
+               </div>
+
+
+               <img
+                   src={item_slot.src}
+                   height={100}
+                   width={100}
+                   alt='_item_slot_'
+                   className={`absolute right-0 bottom-0`}
+       	        style={{right: '10px', bottom: '10px'}}
+               />
+               <img
+                   src={_tools[_current_selected_tool]}
+                   height={100}
+                   width={100}
+                   alt='_item_'
+       	        id='tool'
+                   className={`absolute right-0 bottom-0 hover:transition duration-200 hover:scale-125 flex justify-center items-center`}
+       	        style={{right: '10px', bottom: '10px'}}
+               />
+       	    <img
+                   src={grass_tile.src}
+                   height={100}
+                   width={100}
+                   alt='_building_'
+                   className={`building hidden absolute bottom-0`}
+       	        style={{right: '110px', bottom: '10px'}}
+       	    />
+       	    <img
+                   src={_buildings[_current_building][0]}
+                   height={100}
+                   width={100}
+                   id='bbutton'
+                   alt='_building_'
+                   className={`building hidden absolute bottom-0`}
+       	       style={{right: '110px', bottom: '10px'}}
+       	    />
+            </>)
 }
--- components/Tiles.tsx
+++ components/Tiles.tsx
@@ -30,150 +30,168 @@ import pine  from '@/public/pine_tile.webp';
 import tree  from '@/public/tree_tile.webp';
 import tent  from '@/public/tent_tile.webp';
 import rock  from '@/public/rock_tile.webp';
+import none  from '@/public/none.webp';
 
 
 export default function Tiles() {
     const _MAX_TILE_COLS: string = 'grid-cols-6';
     
     const _tiles_map = [
-	grass.src , rock.src , pine.src  , cherry.src, pine.src , tent.src,
-	tree.src  , grass.src, cherry.src, tree.src  , grass.src, pine.src,
-	rock.src  , pine.src , grass.src , cherry.src, rock.src , tree.src,
-	cherry.src, pine.src , rock.src  , grass.src , rock.src , tree.src
+		none.src  , rock.src, pine.src  , cherry.src, pine.src , tent.src,
+		tree.src  , rock.src, cherry.src, tree.src  , none.src , pine.src,
+		rock.src  , pine.src, none.src  , cherry.src, rock.src , tree.src,
+		cherry.src, pine.src, rock.src  , none.src  , rock.src , tree.src
     ];
     
     // TODO: perhaps there are better ways to do the game loop.
     useEffect(() => {
-	const _buildings: Array = GetBuildings();
-	
-	var _tiles = document.getElementsByClassName('tile');
-	var _current_buildings: number = GetCurrentBuilding();
-	var _items: Dictionary = GetItems();
-    
-	setInterval(() => {
-	    for (let _i = 0; _i < _tiles.length; _i++) {
-		if (!_tiles[_i].classList.contains('preview')) {
-		    // TODO: find a better way to make this logic.
-		    switch (_tiles[_i].alt) {
-			// pine growing; 
-			case pine_saplings_tile0.src:
-			    _tiles[_i].src = pine_saplings_tile1.src;
-			    _tiles[_i].alt = pine_saplings_tile1.src;
-			    break;
-			case pine_saplings_tile1.src:
-			    _tiles[_i].src = pine_saplings_tile2.src;
-			    _tiles[_i].alt = pine_saplings_tile2.src;
-			    break;
-			case pine_saplings_tile2.src:
-			    _tiles[_i].src = pine.src;
-			    _tiles[_i].alt = pine.src;
-			    break;
-			    
-			// tree growing; 
-			case tree_saplings_tile0.src:
-			    _tiles[_i].src = tree_saplings_tile1.src;
-			    _tiles[_i].alt = tree_saplings_tile1.src;
-			    break;
-			case tree_saplings_tile1.src:
-			    _tiles[_i].src = tree_saplings_tile2.src;
-			    _tiles[_i].alt = tree_saplings_tile2.src;
-			    break;
-			case tree_saplings_tile2.src:
-			    _tiles[_i].src = tree.src;
-			    _tiles[_i].alt = tree.src;
-			    break;
-		    }
-		}
-	    }
-	}, 5000);
-	
-	for(let _i = 0; _i < _tiles.length; _i++) {
-	    const _tile = _tiles[_i];
-	    let _tmp; 
-	    
-	    // that's a fucking mess
-	    // tile preview when the hammer is selected 
-	    _tiles[_i].parentElement.onmouseover = () => {
-		_current_buildings = GetCurrentBuilding();
-		_tmp = _tile.alt;
-		
-		if (GetCurrentTool() === 0) {
-		    if (_tile.alt === grass.src) {
-			if (_items['wood'] >= _buildings[_current_buildings][1]['wood'] && _items['rock'] >= _buildings[_current_buildings][1]['rock']) {
-			    _tile.src = _buildings[_current_buildings][0];
-			    _tile.alt = _buildings[_current_buildings][0];
-			    
-			    _tile.classList.add('preview');
+		const _buildings: Array = GetBuildings();
+
+		var _tiles = document.getElementsByClassName('tile');
+		var _current_buildings: number = GetCurrentBuilding();
+		var _items: Dictionary = GetItems();
+
+		setInterval(() => {
+			for (let _i = 0; _i < _tiles.length; _i++) {
+				if (!_tiles[_i].classList.contains('preview')) {
+					// TODO: find a better way to make this logic.
+					switch (_tiles[_i].alt) {
+							// pine growing;
+						case pine_saplings_tile0.src:
+							_tiles[_i].src = pine_saplings_tile1.src;
+							_tiles[_i].alt = pine_saplings_tile1.src;
+							break;
+						case pine_saplings_tile1.src:
+							_tiles[_i].src = pine_saplings_tile2.src;
+							_tiles[_i].alt = pine_saplings_tile2.src;
+							break;
+						case pine_saplings_tile2.src:
+							_tiles[_i].src = pine.src;
+							_tiles[_i].alt = pine.src;
+							break;
+
+							// tree growing;
+						case tree_saplings_tile0.src:
+							_tiles[_i].src = tree_saplings_tile1.src;
+							_tiles[_i].alt = tree_saplings_tile1.src;
+							break;
+						case tree_saplings_tile1.src:
+							_tiles[_i].src = tree_saplings_tile2.src;
+							_tiles[_i].alt = tree_saplings_tile2.src;
+							break;
+						case tree_saplings_tile2.src:
+							_tiles[_i].src = tree.src;
+							_tiles[_i].alt = tree.src;
+							break;
+					}
+				}
 			}
-		    }
-		}
-	    }
-	    _tiles[_i].parentElement.onmouseout = () => {
-		if (GetCurrentTool() === 0) {
-		    if (_tile.classList.contains('preview')) {
-			_tile.src = _tmp;
-			_tile.alt = _tmp;
-			
-			_tile.classList.remove('preview');
-		    }
-		}
-	    }
-	    // Building and removing tiles
-	    _tiles[_i].parentElement.onmousedown = () => {
-		if (GetCurrentTool() === 1) {
-		    switch (_tile.alt) {
-			case rock.src:
-			    _tile.src = grass.src;
-			    _tile.alt = grass.src;
-			    AddItem('rock', 1);
-			    break;
-			case pine.src:
-			case tree.src:
-			    _tile.src = grass.src;
-			    _tile.alt = grass.src;
-			    AddItem('wood', 1);
-			    break;
-			case cherry.src:
-			    _tile.src = naked_cherry.src;
-			    _tile.alt = naked_cherry.src;
-			    break;
-		    }
-		} 
-		else if (GetCurrentTool() === 0) {
-		    if (_items['wood'] >= _buildings[_current_buildings][1]['wood'] && _items['rock'] >= _buildings[_current_buildings][1]['rock']) {
-			if (_tmp === grass.src) {
-			    _tile.src = _buildings[_current_buildings][0];
-			    _tile.alt = _buildings[_current_buildings][0];
-			    RemoveItem('wood', _buildings[_current_buildings][1]['wood']);
-			    RemoveItem('rock', _buildings[_current_buildings][1]['rock']);
-			    _tmp = _buildings[_current_buildings][0];
+		}, 5000);
+
+		for(let _i = 0; _i < _tiles.length; _i++) {
+			const _tile = _tiles[_i];
+			let _tmp;
+
+			// that's a fucking mess
+			// tile preview when the hammer is selected
+			_tile.parentElement.onmouseover = () => {
+				_current_buildings = GetCurrentBuilding();
+				_tmp = _tile.alt;
+
+				if (GetCurrentTool() === 0) {
+					if (_tile.alt === none.src) {
+						if (_items['wood'] >= _buildings[_current_buildings][1]['wood'] && _items['rock'] >= _buildings[_current_buildings][1]['rock']) {
+							_tile.src = _buildings[_current_buildings][0];
+							_tile.alt = _buildings[_current_buildings][0];
+
+							_tile.classList.add('preview');
+						}
+					}
+				}
 			}
-		    }
+			_tile.parentElement.onmouseout = () => {
+				if (GetCurrentTool() === 0) {
+					if (_tile.classList.contains('preview')) {
+						_tile.src = _tmp;
+						_tile.alt = _tmp;
+
+						_tile.classList.remove('preview');
+					}
+				}
+			}
+			// Building and removing tiles
+			_tile.parentElement.onmousedown = () => {
+				if (GetCurrentTool() === 1) {
+					switch (_tile.alt) {
+						case rock.src:
+							_tile.src = none.src;
+							_tile.alt = none.src;
+							AddItem('rock', 1);
+							break;
+						case pine.src:
+						case tree.src:
+							_tile.src = none.src;
+							_tile.alt = none.src;
+							AddItem('wood', 1);
+							break;
+						case cherry.src:
+							_tile.src = naked_cherry.src;
+							_tile.alt = naked_cherry.src;
+							break;
+					}
+				}
+				else if (GetCurrentTool() === 0) {
+					if (_items['wood'] >= _buildings[_current_buildings][1]['wood'] && _items['rock'] >= _buildings[_current_buildings][1]['rock']) {
+						if (_tmp === none.src) {
+							_tile.src = _buildings[_current_buildings][0];
+							_tile.alt = _buildings[_current_buildings][0];
+							RemoveItem('wood', _buildings[_current_buildings][1]['wood']);
+							RemoveItem('rock', _buildings[_current_buildings][1]['rock']);
+							_tmp = _buildings[_current_buildings][0];
+						}
+					}
+				}
+			};
 		}
-	    };
-	}
     }, []);
     
     return (
-	<main className={`grid ${_MAX_TILE_COLS} gap-1 flex justify-center`}>
-	    {
-		_tiles_map.map((_i, _j) => {
-		    return (
-			<div key={_j} className={`min-h-25 min-w-25 relative rounded-x1 hover:transition duration-200 hover:scale-125`}>
-			    <img
-			        height={100}
-			        width={100}
-			        
-			        src={_i}
-			        alt={_i}
-			        
-			        className={`tile pointer-events-none absolute bottom-0 min-h-25 min-w-25`}
-			    />
-			</div>
-		    )
-		})
-	    }
-	</main>
+		<main className={`grid ${_MAX_TILE_COLS} gap-1 flex justify-center`}>
+			{
+				_tiles_map.map((_i, _j) => {
+					const _columns: number = 6;
+					const _r = Math.floor(_j / _columns);
+					const _c = _j % _columns;
+
+					const _darker = (_r + _c) % 2 !== 0;
+					return (
+						<div key={_j} className={`tilep min-h-25 min-w-25 relative rounded-x1 hover:transition duration-200 hover:scale-125`}>
+							<img
+						        height={100}
+						        width={100}
+
+						        src={grass.src}
+						        alt={grass.src}
+
+						        className={`grass pointer-events-none absolute bottom-0 min-h-25 min-w-25`}
+						        style={{
+									filter: _darker ? 'brightness(100%)' : 'brightness(60%)'
+								}}
+							/>
+							<img
+						        height={100}
+						        width={100}
+
+						        src={_i}
+						        alt={_i}
+
+						        className={`tile pointer-events-none absolute bottom-0 min-h-25 min-w-25`}
+							/>
+							</div>
+					)
+				})
+			}
+		</main>
     )
 }
 // absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-3/5
